50b5e2cf941eb4d7b842a9e2dd56254a766a6314
HemantSoniTech <65235665+HemantSoniTech@users.noreply.github.com>
2021-02-07 12:55:50 +0530

    Dot net 3.0

--- JWTTokenWebAPI/Program.cs
+++ JWTTokenWebAPI/Program.cs
@@ -1,10 +1,9 @@
-﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,12 +13,14 @@ namespace JWTTokenWebAPI
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
--- JWTTokenWebAPI/Startup.cs
+++ JWTTokenWebAPI/Startup.cs
@@ -1,11 +1,13 @@
-﻿using JWTTokenWebAPI.Helpers;
+using JWTTokenWebAPI.Helpers;
 using JWTTokenWebAPI.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,31 +17,51 @@ namespace JWTTokenWebAPI
 {
     public class Startup
     {
-        public IConfiguration Configuration { get; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
+        {//New
             services.AddCors();
             services.AddControllers();
-
             // configure strongly typed settings object
             services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
 
             // configure DI for application services
             services.AddScoped<IUserService, UserService>();
 
-            //services.AddMvc();
+            // Register Swagger  
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Sample API", Version = "version 1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+
+                // Enable middleware to serve generated Swagger as a JSON endpoint.  
+                app.UseSwagger();
+
+                // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),  
+                // specifying the Swagger JSON endpoint.  
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                });
+            }
+
+            app.UseHttpsRedirection();
+
             app.UseRouting();
 
             // global cors policy
@@ -48,17 +70,15 @@ namespace JWTTokenWebAPI
                 .AllowAnyMethod()
                 .AllowAnyHeader());
 
-            if (env.EnvironmentName== "Development")
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseAuthorization();
 
             // custom jwt auth middleware
             app.UseMiddleware<JwtAuthMiddleware>();
 
-            app.UseEndpoints(x => x.MapControllers());
-
-            //app.UseMvc();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
