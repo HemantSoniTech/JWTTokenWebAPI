6228f9ffced0fa45450b2879bd71e88a36327996
HemantSoniTech <65235665+HemantSoniTech@users.noreply.github.com>
2021-02-07 11:32:35 +0530

    Added Jwt Authemntication Project

---
+++ JWTTokenWebAPI/Services/UserService.cs
@@ -0,0 +1,67 @@
+﻿using JWTTokenWebAPI.Entities;
+using JWTTokenWebAPI.Helpers;
+using JWTTokenWebAPI.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTTokenWebAPI.Services
+{
+    public interface IUserService
+    {
+        AuthenticateResponse Authenticate(AuthenticateRequest model);
+        IEnumerable<User> GetAll();
+        User GetById(int id);
+    }
+    public class UserService : IUserService
+    {
+        private List<User> _users = new List<User>()
+        {
+            new User(){ Id=1, FirstName="Hemant", LastName="Soni", Username="HSoni", Password="hemantsoni"}
+        };
+        private readonly AppSettings _appSettings;
+        public UserService(IOptions<AppSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+        }
+        public AuthenticateResponse Authenticate(AuthenticateRequest model)
+        {
+            var user = _users.SingleOrDefault(u => u.Username == model.Username && u.Password == model.Password);
+
+            // return null if user not found
+            if (user == null) return null;
+
+            // authentication successful so generate jwt token
+            var token = GenerateJwtToken(user);
+
+            return new AuthenticateResponse(user, token);
+        }
+        public IEnumerable<User> GetAll()
+        {
+            return _users;
+        }
+        public User GetById(int id)
+        {
+            return _users.FirstOrDefault(x => x.Id == id);
+        }
+        private string GenerateJwtToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var secretKey = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim("id", user.Id.ToString()) }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(secretKey), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
---
+++ JWTTokenWebAPI/Startup.cs
@@ -0,0 +1,64 @@
+﻿using JWTTokenWebAPI.Helpers;
+using JWTTokenWebAPI.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTTokenWebAPI
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddControllers();
+
+            // configure strongly typed settings object
+            services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+
+            // configure DI for application services
+            services.AddScoped<IUserService, UserService>();
+
+            //services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            // global cors policy
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+            if (env.EnvironmentName== "Development")
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // custom jwt auth middleware
+            app.UseMiddleware<JwtAuthMiddleware>();
+
+            app.UseEndpoints(x => x.MapControllers());
+
+            //app.UseMvc();
+        }
+    }
+}
